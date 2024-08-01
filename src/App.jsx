be484ad72c9642f5dad4baa4b5ae76a3fccd7526
Yash <yashgarg1703@gmail.com>
2024-08-02 00:49:55 +0530

    YASH_GARG

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import "./App.css";
-import Result from "./components/Result";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from './pages/Header';
+import Home from './pages/Home';
+import PlanTrip from './pages/PlanTrip'
+import Contact from "./pages/Contact";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import './App.css';
 function App() {
   return (
-    <>
-      <div>
-        <Result />
-      </div>
-    </>
+    <div className="App">
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/planTrip" element={<PlanTrip/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
