d9a575fa1ba00919a7f7b160ea7c23c17c92b38b
shailendra14r <shailendrarajput0860@gmail.com>
2024-08-03 20:01:56 +0530

    done

--- src/components/Trip/DayPlan.jsx
+++ src/components/Trip/DayPlan.jsx
@@ -5,20 +5,29 @@ export default function DayPlan({day}) {
   
   return (
     <div>
-      <h3>Day 1 : {day.area}</h3>
+      <h3>Day {day.day}</h3>
       <div className="schedule-grid">
         {
-          day.locations.map((location) => <LocationCard 
-                key = {location.id}
-                name = {location.name}
-                description = {location.description}
-                time = {location.time} 
-                duration = {location.duration}
-                cost = {location.cost}
-                img = {location.img}
+          day.places.map((place, idx) => <LocationCard 
+                key = {idx}
+                name = {place.placeName}
+                description = {place.description}
+                time = {place.bestTimeToVisit} 
+                duration = {place.timeToTravel}
+                cost = {place.ticketPricing}
+                // img = {place.placeImageUrl}
+                img = {img_url[(Math.ceil(Math.random()*100)) % 4]}
            />)
         }
       </div>
     </div>
   );
 }
+
+
+const img_url = [
+  "https://images.unsplash.com/photo-1661197884083-ab22b7ad9204?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D",
+  "https://images.unsplash.com/photo-1455587734955-081b22074882?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWx8ZW58MHx8MHx8fDA%3D",
+  "https://media.istockphoto.com/id/1078903506/photo/rooftop-image-of-buildings-and-neighboring-community-mumbai-maharashtra.webp?b=1&s=170667a&w=0&k=20&c=TLMBLoSm3swggjMvflCEDEcPO-jJTMc08GWcMwYbQ4k=",
+  "https://images.unsplash.com/photo-1499174549139-68d3f37243b4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dG93bnxlbnwwfHwwfHx8MA%3D%3D"
+];
--- src/components/Trip/LocationCard.jsx
+++ src/components/Trip/LocationCard.jsx
@@ -3,15 +3,15 @@ import React from "react";
 export default function LocationCard({name, description, time, duration, cost, img}) {
   return (
     <div className="location">
-      <div className="time">{time}</div>
-      <div className="flex-row location-card">
-        <img src={img} />
+      <div className="time">{duration}</div>
+      <div className="location-card">
+        <img src={img} /> 
         <div className="location-details">
           <h6 className="location-name">{name}</h6>
           <p>
             {description}
           </p>
-          <div>⏱ {duration}</div>
+          <div>⏱{time}</div>
           <div>🎟️ {cost}</div>
         </div>
       </div>
